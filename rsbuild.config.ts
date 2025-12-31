3c5d9be934c40236aadb408c7f3b3791bb63d7d2
caibin <caibin@bilibili.com>
2025-12-31 11:24:33 +0800

    chore: appid 读取

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: {
+      'import.meta.env.VITE_FEISHU_APP_ID': JSON.stringify(
+        process.env.VITE_FEISHU_APP_ID || ''
+      ),
+    },
+  },
 });
